a0b9611132eeed0cba9808c2ee5dc04bbea2efcb
newfivefour <newfivefour@newfivefour.com>
2016-08-10 01:08:59 +0200

    Change namespace to cipetpet.android!
    Updated some tools since AS told me to.

--- app/src/androidTest/java/cipetpet/android/ApplicationTest.java
+++ app/src/androidTest/java/cipetpet/android/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.newfivefour;
+package cipetpet.android;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- app/src/test/java/cipetpet/android/ExampleUnitTest.java
+++ app/src/test/java/cipetpet/android/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.newfivefour;
+package cipetpet.android;
 
 import org.junit.Test;
 
