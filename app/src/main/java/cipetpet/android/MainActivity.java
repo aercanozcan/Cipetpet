2d2db81cbc2080e8de66c8c1811efb9130584665
newfivefour <newfivefour@newfivefour.com>
2016-08-11 12:53:14 +0200

    Integrated HockeyApp.

--- app/src/main/java/cipetpet/android/MainActivity.java
+++ app/src/main/java/cipetpet/android/MainActivity.java
@@ -2,6 +2,8 @@ package cipetpet.android;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import net.hockeyapp.android.CrashManager;
+import net.hockeyapp.android.UpdateManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //random shite!
+
+        checkForUpdates();
+    }
+
+    @Override
+    public void onResume() {
+      super.onResume();
+      checkForCrashes();
+    }
+
+    @Override
+    public void onPause() {
+      super.onPause();
+      unregisterManagers();
+    }
+
+    @Override
+    public void onDestroy() {
+      super.onDestroy();
+      unregisterManagers();
+    }
+
+    private void checkForCrashes() {
+      CrashManager.register(this);
+    }
+
+    private void checkForUpdates() {
+      // TODO: Remove this for store builds!
+      UpdateManager.register(this);
+    }
+
+    private void unregisterManagers() {
+      UpdateManager.unregister();
     }
 }
